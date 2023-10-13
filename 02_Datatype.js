56581033a64fdcc6532c51b1ba69cdc7ce087add
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:08:35 +0530

    Add files via upload

---
+++ 02_Datatype.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert(3+3) it can not used on browser
+
+console.log(2 + 2);
+console.log("hello");
+let name="Vikas"  //strings 
+let age=18  //integer
+let isLoggedin=false
+let state =null //undefined datatype
+
+//sumbole //primitive datatype that is unique
+console.table([name,age,isLoggedin,state])
+console.log(typeof name)
+console.log(typeof null)
+console.log("hello")
