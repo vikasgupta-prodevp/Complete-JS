401837988e695d82fe60461a0dd6bfafee1d4043
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:13:38 +0530

    Add files via upload

---
+++ 11_Time_Date.js
@@ -0,0 +1,21 @@
+let mydate = new Date()
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toISOString())
+console.log(mydate.toJSON())
+console.log(mydate.toLocaleString())
+console.log(mydate.toTimeString())
+console.log(mydate.getTimezoneOffset())
+console.log(mydate.toUTCString())
+console.log(mydate.setUTCMinutes(1))
+
+
+console.log(typeof mydate)
+
+
+let mycdate=new Date("2023-01-24")
+console.log(mycdate.toLocaleString())
+console.log(mycdate.getHours())
+console.log(mycdate.getMinutes())
+console.log(mycdate.getUTCDay())
+console.log(m)
