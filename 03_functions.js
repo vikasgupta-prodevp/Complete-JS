63f0aa986e96c8c6b97f072188f197b75485dce6
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:09:00 +0530

    Add files via upload

---
+++ 03_functions.js
@@ -0,0 +1,31 @@
+// let iscore=false;
+
+// console.log(typeof iscore);
+// console.log(typeof (iscore));
+
+// let valueinNumber=Number(iscore);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+//***************Operations**********
+let vallue=7;
+let negvalue=-vallue;
+console.log(negvalue)
+console.log(2+2)
+console.log(2*2)
+console.log(2-5)
+console.log(2/8)
+
+
+let str1="Hello"
+let str2=" Vikas"
+let str3=str1+str2
+console.log(str3)
+
+
+console.log("1"+1)
+console.log(1+1+"5")
+console.log(1+"1")
+console.log("1"+"1")
+console.log("Hello world")
