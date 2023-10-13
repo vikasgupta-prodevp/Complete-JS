fd0382491a74b0130384ed173acefa4f9f258698
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:08:03 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountID=144555
+let accountEmail="vikasgidf454@gmail.com"
+var accountPassword="54666"
+accoountCity="Itwa"
+let accountState
+
+// accountID= 2 *****cant't change******
+
+accountEmail="dshfjkas@gmail.com"
+accountPassword="jdfhaskj"
+accoountCity="Bangluru"
+console.log(accountEmail)
+console.table([accountID,accountPassword,accoountCity,accountState]);
