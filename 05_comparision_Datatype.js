c07f75928ded74d765e5cb708b790d37da07230f
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:12:05 +0530

    Add files via upload

---
+++ 05_comparision_Datatype.js
@@ -0,0 +1,18 @@
+// console.log("3">2);
+// console.log("3"<2);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+const score=100;
+const scoreValue=1009;
+const isloggedIn=false;
+const istemp=null;
+let userEmail;
+
+const id=Symbol('123');
+const isanotherid= Symbol('123');
+console.log(id==isanotherid);
