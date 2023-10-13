5e8df8021208f74f0a9d2e3295e4730fc1681995
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:12:18 +0530

    Add files via upload

---
+++ 06_Array.js
@@ -0,0 +1,20 @@
+const score=100;
+const scoreValue=1009;
+const isloggedIn=false;
+
+const heros=["Shaktiman","Naagraj","Doga"];
+let myobj={
+    name:"Vikas",
+    age:34,
+}
+
+
+const myfunction=function(){
+    console.log("Hello world");
+}
+
+
+console.log(typeof myfunction);
+console.log(heros);
+console.log(myobj.name);
+console.log(myobj.age);
