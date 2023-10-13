9b1159c726f1019c0bffefa59c3a5f4c972330da
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:13:24 +0530

    Add files via upload

---
+++ 10_rounof.js
@@ -0,0 +1,37 @@
+const hundres = 100000
+console.log(hundres.toLocaleString())
+console.log(typeof (hundres))
+console.log(Math)
+console.log(Math.abs(hundres))
+console.log(Math.abs(-4))
+console.log(Math.LOG10E)
+console.log(Math.LN10)
+console.log(Math.LOG2E)
+console.log(Math.SQRT2)
+console.log(Math.acos)
+function calcAngle(adjacent, hypotenuse) {
+    return Math.acos(adjacent / hypotenuse);
+}
+console.log(calcAngle)
+
+console.log(Math.hypotenuse)
+console.log(Math.cbrt(10))
+console.log(Math.cbrt(27))
+console.log(Math.ceil(9.5))
+console.log(Math.ceil(9.9))
+console.log(Math.floor(8.9))
+console.log(Math.trunc(10.1))
+console.log(Math.sqrt(9))
+console.log(Math.PI)
+console.log(Math.SQRT1_)
+console.log(Math.min(1, 2, 3, 4, 5))
+console.log(Math.max(1, 2, 3, 4, 5))
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+const min = 1
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
