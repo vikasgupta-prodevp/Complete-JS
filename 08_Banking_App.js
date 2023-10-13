c5a044dcc2c49179473e3d13cebb0986222aeebf
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-14 00:12:45 +0530

    Add files via upload

---
+++ 08_Banking_App.js
@@ -0,0 +1,16 @@
+const score=400
+const balance = new Number(100) 
+console.log(balance)
+console.log(score)
+console.log(balance.toString().length)
+console.log(balance.toFixed(3))
+console.log(balance.toFixed(0))
+
+const otherNumber=525.321;
+console.log(otherNumber.toFixed(12));     
+console.log(otherNumber.toLocaleString());
+console.log(otherNumber.toPrecision(1));     
+
+
+const hundred=123456
+comsole.log(hundred.toLocaleString('en-IN'))
