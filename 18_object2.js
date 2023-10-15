99f209df8c03d004f833ffda658bbaa05ea4b482
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-15 23:47:48 +0530

    Add files via upload

---
+++ 18_object2.js
@@ -0,0 +1,55 @@
+const tinderUser1 = {}
+tinderUser1.id = "123abc"
+tinderUser1.name = "Vikas"
+tinderUser1.isLoggedIn = false
+
+
+// console.log(tinderUser)
+const tinderUser2 = {
+    email: "emal.com",
+    fullname: {
+        userfullname: {
+            firstname: "Vikas",
+            lastname: "Kumar"
+        }
+    }
+}
+// const tinderUser3={...tinderUser1,...tinderUser2}
+// console.log(tinderUser3)
+
+
+const users=[
+    {
+        id:1,
+        email:"Gl@gmail.com"
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+// users[1].id
+// console.log(tinderUser1)
+
+
+// console.log(Object.keys(tinderUser1))
+// console.log(Object.values(tinderUser1))
+console.log(Object.entries(users))
+
+
+
+// console.log(tinderUser2.fullname.userfullname.firstname)
+// console.log(tinderUser2.fullname.userfullname)
+// console.log(tinderUser2.userfullname)
+// console.log(tinderUser2.email)
+
+// const otheruser={tinderUser1,tinderUser2}
+// const otheruser=Object.assign(tinderUser1,tinderUser2)
+
+
+// const otheruser=Object.assign({},tinderUser1,tinderUser2)
+
+// console.log(otheruser)
