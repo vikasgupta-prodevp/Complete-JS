604e12d5fa8615af1a4da2c9f4ae51f5bf6474f8
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-15 20:42:33 +0530

    Add files via upload

---
+++ 15_Slice_splice.js
@@ -0,0 +1,16 @@
+const myarr=[1,2,3,4,5,6,7,8,9]
+console.log(myarr)
+
+
+//slice in javascript
+const mynewarr1=myarr.slice(1,3)
+console.log(myarr)
+console.log("A",mynewarr1)
+
+
+//splice in javascript
+const mynewarr2=myarr.splice(1,3)
+console.log(myarr)
+console.log("B",mynewarr2)
+
+
