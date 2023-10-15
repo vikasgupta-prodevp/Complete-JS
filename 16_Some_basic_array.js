bb243236ba74886731d646883b00b5001773c71a
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-15 22:15:11 +0530

    Add files via upload

---
+++ 16_Some_basic_array.js
@@ -0,0 +1,49 @@
+const heroes1 = ["Thor", "ironman", "Spiderman"]
+const heroes2 = ["Batman", "Flash", "Superman"]
+
+
+// heroes1.push(heroes2)
+// console.log(heroes1)
+// console.log(heroes1[3])
+// console.log(heroes1[3][1])
+// console.log(heroes1[3][2])
+
+
+// const all = heroes1.concat(heroes2)
+// console.log(all)
+// console.log(all[3])
+
+
+// const allh=[...heroes1,...heroes2]
+// console.log(allh)
+
+
+
+// const anarr=[1,2,3,[4,5,6],[7,[8,9,74,52]]];
+// const real=anarr.flat(Infinity);
+// console.log(real);
+
+
+
+
+// const arr=[1,2,3,4,[,4,5,6],[10,20,[30,40,50]]];
+// const an_arr=arr.flat(3)
+// console.log(an_arr)
+
+
+
+// console.log(Array.isArray("Vikas"));
+// const arr=Array.from("Vikas");
+// const arr1=Array.length("vikas");
+// console.log(arr1)
+
+
+// console.log(Array.isArray("Rakesh"));
+// console.log(Array.from("Rakesh"));
+
+
+console.log(Array.from({name:"Vikas"}));
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3))
