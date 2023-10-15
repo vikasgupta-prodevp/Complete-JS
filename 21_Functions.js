26fc6a7f95c94b3ec6d52f4a26bb48940e00219e
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-16 00:59:47 +0530

    Update 21_Functions.js

--- 21_Functions.js
+++ 21_Functions.js
@@ -1,56 +1,55 @@
-// function myname(){
-//     console.log("V"),
-//     console.log("I"),
-//     console.log("K"),
-//     console.log("A"),
-//     console.log("S")
-// };
-// myname();
+ function myname(){
+     console.log("V"),
+     console.log("I"),
+     console.log("K"),
+     console.log("A"),
+     console.log("S")
+ };
+ myname();
 
 
-// function addnum(num1,num2){
-// console.log(num1+num2)
-// }
+ function addnum(num1,num2){
+ console.log(num1+num2)
+ }
 
 
-// function addnum(num1,num2){
-// // let result=num1+num2
-// // return result
-// return num1+num2
-// }
-// const result=addnum(8,8)
-// console.log("Result :",result)
+ function addnum(num1,num2){
+ let result=num1+num2
+ return result
+ return num1+num2
+     }
+ const result=addnum(8,8)
+ console.log("Result :",result)
 
 
-// function mul(num1,num2){
-//     return num1*num2
-// };
-// const result=mul(4,5);
-// console.log("Result:",result);
-
-
-
-// function diff(num1,num2){
-//     return num1-num2
-// };
-// const result=diff(4,5);
-// console.log("Result:",result);
+ function mul(num1,num2){
+     return num1*num2
+ };
+ const result=mul(4,5);
+ console.log("Result:",result);
 
 
 
-// function div(num1,num2){
-//     return num1/num2
-// };
-// const result=div(4,5);
-// console.log("Result:",result);
+ function diff(num1,num2){
+     return num1-num2
+ };
+ const result=diff(4,5);
+ console.log("Result:",result);
 
 
-// function loginUserMessage(Username){
-//     return `${Username} just loffed in`
-// }
+ function div(num1,num2){
+     return num1/num2
+ };
+ const result=div(4,5);
+ console.log("Result:",result);
 
-// const result=loginUserMessage("Vikas");
-// console.log(result)
+
+ function loginUserMessage(Username){
+     return `${Username} just loffed in`
+ }
+
+ const result=loginUserMessage("Vikas");
+ console.log(result)
 
 
 
