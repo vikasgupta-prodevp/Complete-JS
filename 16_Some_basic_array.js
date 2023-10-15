04fd2d3f61df81b3a579928136cbc7f6d02d71d7
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-15 22:16:39 +0530

    Update 16_Some_basic_array.js

--- 16_Some_basic_array.js
+++ 16_Some_basic_array.js
@@ -1,45 +1,43 @@
 const heroes1 = ["Thor", "ironman", "Spiderman"]
 const heroes2 = ["Batman", "Flash", "Superman"]
 
-
-// heroes1.push(heroes2)
-// console.log(heroes1)
-// console.log(heroes1[3])
-// console.log(heroes1[3][1])
-// console.log(heroes1[3][2])
-
-
-// const all = heroes1.concat(heroes2)
-// console.log(all)
-// console.log(all[3])
+ heroes1.push(heroes2)
+ console.log(heroes1)
+ console.log(heroes1[3])
+ console.log(heroes1[3][1])
+ console.log(heroes1[3][2])
 
 
-// const allh=[...heroes1,...heroes2]
-// console.log(allh)
+ const all = heroes1.concat(heroes2)
+console.log(all)
+ console.log(all[3])
 
 
-
-// const anarr=[1,2,3,[4,5,6],[7,[8,9,74,52]]];
-// const real=anarr.flat(Infinity);
-// console.log(real);
+ const allh=[...heroes1,...heroes2]
+ console.log(allh)
 
 
 
-
-// const arr=[1,2,3,4,[,4,5,6],[10,20,[30,40,50]]];
-// const an_arr=arr.flat(3)
-// console.log(an_arr)
-
+ const anarr=[1,2,3,[4,5,6],[7,[8,9,74,52]]];
+ const real=anarr.flat(Infinity);
+ console.log(real);
 
 
-// console.log(Array.isArray("Vikas"));
-// const arr=Array.from("Vikas");
-// const arr1=Array.length("vikas");
-// console.log(arr1)
 
 
-// console.log(Array.isArray("Rakesh"));
-// console.log(Array.from("Rakesh"));
+ const arr=[1,2,3,4,[,4,5,6],[10,20,[30,40,50]]];
+ const an_arr=arr.flat(3)
+ console.log(an_arr)
+
+
+
+ console.log(Array.isArray("Vikas"));
+ const arr=Array.from("Vikas");
+ const arr1=Array.length("vikas");
+ console.log(arr1)
+
+
+ console.log(Array.isArray("Rakesh"));/ console.log(Array.from("Rakesh"));
 
 
 console.log(Array.from({name:"Vikas"}));
