154466d95105ee5869343567ddf585c713416738
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-17 23:20:46 +0530

    Add files via upload

---
+++ 25_Queston.js
@@ -0,0 +1,11 @@
+//First method to decleraing function 
+function addone(num){
+    return num+1
+}
+addone(10);
+
+// second method to declaring function using variable
+const addtwo=function(num){
+    return num+2
+}
+addtwo(2)//it will thorugh an error when we declare addtwo() before variable const
