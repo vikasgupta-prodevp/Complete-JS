06ef4b281c08fd2c9d19ce6cf75071e3efcb7c28
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-17 23:20:29 +0530

    Add files via upload

---
+++ 24_Nested_Closer.js
@@ -0,0 +1,24 @@
+// function one() {
+//     const username = "Vikas"
+//     function two() {
+//         const website = "Youtube"
+//         console.log(username)
+//     }
+//     // console.log(website)
+//     two()
+// }
+// one()
+
+
+
+if (true) {
+    const username = "Amit"
+    if (username === "Amit") {
+        const website = "Youtube"
+        console.log(username +" "+ website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+//******************** */
