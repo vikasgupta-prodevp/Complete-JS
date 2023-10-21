3230e4140626920145911a2931728fb151b4362c
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-21 23:47:34 +0530

    Add files via upload

---
+++ 31_Iteration.js
@@ -0,0 +1,80 @@
+// for loop
+// for (let x = 1; x <= 10; x++) {
+//     const element = x;
+//     if (x == 5) {
+//         console.log("5 is the best number");
+//     }
+// console.log(element);
+
+// }
+
+
+// console.log(element);
+
+// for (let i = 2; i <= 10; i++) {
+// console.log(`outer value loop ${i}`);
+// for (let j = 2; j <= 10; j++) {
+// console.log(`inner loop is ${j} and outer loop is ${i}`);
+// console.log(i + '*' + j + ' = ' + i * j);
+// document.write("<br/>");
+//     }
+
+// }
+
+
+
+
+// var i;
+// for (i = 10; i >= 0; i = i - 1) {
+//     var s;
+//     for (s = 0; s < i; s = s + 1) {
+//         document.write("*");
+//     }
+//     //i want this to print a new line
+//     document.write("<br/>");
+
+// }
+
+
+// let array = ["Vikas", "Akash", "Ajay"]
+// console.log(array.length);
+// for (let index = 0; index < array.length; ++index) {
+//     const element = array[1];
+//     console.log(element);
+    // console.log(element);
+
+// }
+
+
+
+
+// for (let x = 1; x <= 20; x++) {
+//     if (x==5) {
+//         console.log(`Dectected ${x}`);
+//         break;
+//     }
+//     console.log(`Value of c is ${x}`);
+    
+// }
+for (let x = 1; x <= 20; x++) {
+    if (x==5) {
+        console.log(`Dectected ${x}`);
+        continue;
+    }
+    console.log(`Value of c is ${x}`);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
