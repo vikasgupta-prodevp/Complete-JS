698073902cc9d705e1f04ef9205a787cb9765605
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-21 23:47:16 +0530

    Add files via upload

---
+++ 30_Truthy.js
@@ -0,0 +1,76 @@
+// const userEmail="vi@gupta.ai"
+
+// if (userEmail) {
+//     console.log("got user email")
+// }
+
+// else{
+//     "Don't have email to logged in"
+// }
+
+
+// const userEmail=[]
+// if (userEmail.length===0) {
+//     console.log("Array is empty");
+// }
+
+// const userEmail={}
+// if (Object.keys(userEmail).length===0) {
+//     console.log("Object is empty");
+// }
+
+
+// Nullish coalescing oper
+// val1=10 ?? 20;
+// val2=null ?? 10
+// val3=undefined ?? 10
+// val4=" " ?? 10
+// val5=true ?? 1
+
+
+// console.log(val3);
+
+const month="Feb"
+switch (month) {
+    case "Jan":
+        console.log("January");
+           break;
+
+    case "Feb":
+        console.log("february");
+            break;
+
+    case "Mar":
+        console.log("March");
+           break;
+
+    case "April":
+                console.log("Aprilmo");
+           break;
+
+    case " M":
+        console.log("May");
+           break;
+
+    default:
+        console.log("nothing");
+        break;
+}
+
+
+
+
+
+// var max = 10000000;
+// var arr = new Array(max);
+// const i=0;
+// for (let i = 0; i < max; i++) {
+//   arr[i] = true;
+// }
+// console.log(arr[i]);
+
+// false values
+// false,0,undefined,null,nan," "
+
+// true
+// "0","false"," ",[],{},fundtion(){},
