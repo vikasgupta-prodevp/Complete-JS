dca1f40d743908e8c2767ac332016d889cd6e17b
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-21 23:48:08 +0530

    Add files via upload

---
+++ 33_foreach.js
@@ -0,0 +1,51 @@
+// const code = ["JS", "PY", "C#", "C++", "JAVA", "C"];
+
+
+
+// code.forEach(function (item,index,arr){
+// console.log(item,index,arr);
+// })
+
+
+// code.forEach(function (item){
+// console.log(code);
+// })
+
+// code.forEach((item) => {
+// console.log(code);
+// })
+
+
+
+
+// code.forEach(function (element) {
+    // console.log(element);
+    // console.log(code);
+
+    // console.log(element.length);
+    // console.log(element.indexOf("C#"));
+    // console.log(element.toUpperCase());
+    // console.log(element.toLowerCase());
+
+// });
+
+
+const mycoding =[
+{
+    languagename:"JavaScript",
+    extension:"js"
+},
+{
+    languagename:"Python",
+    extension:"py"
+},
+{
+    languagename:"C++",
+    extension:"cpp"
+}
+]
+
+mycoding.forEach(function(element,index){
+console.log(element.languagename,element.index);
+})
+
