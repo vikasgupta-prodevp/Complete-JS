25157d5f94abb12092865d6f48a642bdc7975e26
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-21 23:47:51 +0530

    Add files via upload

---
+++ 32_While_Do-while.js
@@ -0,0 +1,73 @@
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 20;
+// }
+
+// let array = ["Batman", "Ajay", "Manoj", "Akmai"];
+// let arr = 0;
+// while (arr <= array.length) {
+//     console.log(`Value is ${array[arr]}`);
+//     arr = arr + 1;
+
+// }
+
+
+// let Array = ["Vikas", "Aniket", "Arman", "Akhilesh", "Ajeet", "Akami"];
+// let arr = 0;
+// while (arr < Array.length) {
+//     console.log(`value is ${Array[arr]}`);
+//     arr++
+// }
+
+
+
+
+
+
+//Looping in array
+
+// const array = [1, 2, 3, 4, 5, 6, 4]
+// for (const arr of array) {
+
+    // console.log(arr);
+    // console.log(array);
+// }
+
+
+// let greeting = "hello_and_welcome_to"
+// for (const greet of greeting) {
+    // console.log(greet);
+    // console.log(greeting);
+// }
+
+
+
+//**********Maps*************
+const map = new Map();
+
+   map.set( 'cpp', 1)
+   map.set( 'csharp', 2)
+   map.set( 'py', 3)
+   map.set( 'java', 4)
+
+console.log(map.get('cpp'));
+console.log(map.get('py'));
+
+
+
+
+for (const key in map) {
+    // console.get('py');
+}
+
+// for (const key in Map) {
+    // console.log(mapect[key]);
+// }
+
+
+
+// const pro = ["java", "python", "c++", "c#"];
+// for (const key in pro) {
+    // console.log(pro[key]);
+// }
