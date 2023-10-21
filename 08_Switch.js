762fbbdc3b046d9eff4c6c5fb70140cc6fcfff47
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-21 23:46:55 +0530

    Add files via upload

---
+++ 08_Switch.js
@@ -0,0 +1,22 @@
+const month="Jan"
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+
+    case "Fen":
+        console.log("Feb");
+        break;
+
+    case "March":
+        console.log("March");
+        break;
+
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("nothing");
+        break;
+}
