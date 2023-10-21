3b4f0b53b3eb75887f36d75fd5fafe57b4f3a9e1
Suraj Gupta <143135340+vikasgupta-prodevp@users.noreply.github.com>
2023-10-21 23:45:35 +0530

    Add files via upload

---
+++ 27_IIFE.js
@@ -0,0 +1,30 @@
+// const Isloggedin = true;
+// const temp = 42;
+
+// === type and value check together
+// if(temp<59){
+// if (temp === 40) {
+//     console.log("Temperature is less than 40");
+// }
+// else {
+//     console.log("Temperature is greater than 40");
+// }
+
+// const score=true;
+// if(true){
+//     const power="Fly";
+//     console.log(`Power of score is : ${power}`)
+// }
+
+
+const score=899;
+if(score==='899'){
+    let power="Hello and welcome";
+    console.log(`${power},Vikas`);
+}
+
+
+console.log(`score is ${score}`)
+console.log(typeof(score));
+// const score1=score
+// console.log(typeof(score1));
